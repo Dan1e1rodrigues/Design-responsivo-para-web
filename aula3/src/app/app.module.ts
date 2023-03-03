ad252d1d43be5a77070893f5a96fde7ca8b7268b
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-03-03 19:53:04 -0300

    commit
    Aula de angular

--- aula3/src/app/app.module.ts
+++ aula3/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './views/about/about.component';
 import { Auula3Component } from './ex1/auula3/auula3.component';
 import { Ex2Component } from './ex2/ex2/ex2.component';
+import { Ex03Component } from './views/ex03/ex03.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     Auula3Component,
-    Ex2Component
+    Ex2Component,
+    Ex03Component
   ],
   imports: [
     BrowserModule
