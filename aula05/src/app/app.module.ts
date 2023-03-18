4266938e9d84c833d05e2fa8729d5aa727a77411
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-03-17 22:07:40 -0300

    commit

--- aula05/src/app/app.module.ts
+++ aula05/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { Ex04Component } from './views/ex04/ex04.component';
 import { DuasviasComponent } from './views/duasvias/duasvias.component';
 import { FormsModule } from '@angular/forms';
 import { Duasvias01Component } from './views/duasvias01/duasvias01.component';
+import { Duasvias02Component } from './views/duasvias02/duasvias02.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { Duasvias01Component } from './views/duasvias01/duasvias01.component';
     Ex03Component,
     Ex04Component,
     DuasviasComponent,
-    Duasvias01Component
+    Duasvias01Component,
+    Duasvias02Component
   ],
   imports: [
     BrowserModule,
