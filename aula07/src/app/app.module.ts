e09e1fcd02c030591da2e35043793d32fe27a08d
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-03-31 20:18:42 -0300

    commit

--- aula07/src/app/app.module.ts
+++ aula07/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgIfComponent } from './views/ng-if/ng-if.component';
+import { FormsModule } from '@angular/forms';
+import { Ex05Component } from './views/ex05/ex05.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgIfComponent,
+    Ex05Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
