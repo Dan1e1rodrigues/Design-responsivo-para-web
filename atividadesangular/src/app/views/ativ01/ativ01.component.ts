4e1ec441b74955ada159d0f21122f464af73ef5e
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-04-06 10:59:42 -0300

    commit
    Property binding e Interpolação

--- atividadesangular/src/app/views/ativ01/ativ01.component.ts
+++ atividadesangular/src/app/views/ativ01/ativ01.component.ts
@@ -7,11 +7,22 @@ import { Component } from '@angular/core';
 })
 export class Ativ01Component {
 
+  // Criando os atributos de imagem colocando o caminho das imagens para assim serem chamadas no HTML.
   luffy = 'assets/luffy.jpg';
 
-  zoro = 'assets/zoro.jpg'
+  zoro = 'assets/zoro.jpg';
 
-  sanji = 'assets/sanji.jpg'
+  sanji = 'assets/sanji.jpg';
+
+// ----------------------------------------------------------------------------------------
+
+//  Feito isso chamo meu class que criei no HTML e nomeio ele para editar o style
+
+  styleluffy = 'luffy';
+
+  stylezoro = 'zoro';
+
+  stylesanji = 'sanji';
   
   constructor(){
 
